26c5074e73ba917a1bcb453e9446c7a783e7556b
Ron Barrantes <ronbarrantes@gmail.com>
2017-07-27 15:19:57 -0700

    adding info

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 'use strict';
 
-function Customer(product, quantity, customer, street, city,
+var productNamesArr = [
+  'bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg',
+  'breakfast.jpg', 'bubblegum.jpg',
+  'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg',
+  'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg',
+  'scissors.jpg', 'shark.jpg', 'sweep.png',
+  'tauntaun.jpg', 'unicorn.jpg', 'usb.gif',
+  'water-can.jpg', 'wine-glass.jpg'
+];
+
+Order.all = []; // keep it safe
+
+function Order(product, quantity, customer, street, city,
   state, zip, phonenumber, creditcard) {
   this.product = product;
   this.quantity = quantity;
@@ -10,9 +22,8 @@ function Customer(product, quantity, customer, street, city,
   this.state = state;
   this.phonenumber = phonenumber;
   this.creditcard = creditcard;
-
+  Order.all.push(this);
 }
 
-
-new Customer('ball', '3', 'Bob Robertsonburgton',
-'123 Paper St.', 'Seattle','WA','98122', '123-456-7890', '4400 1234 5678 9123');
+new Customer(productNamesArr[1], '3', 'Bob Robertsonburgton',
+  '123 Paper St.', 'Seattle', 'WA', '98122', '123-456-7890', '4400 1234 5678 9123');
