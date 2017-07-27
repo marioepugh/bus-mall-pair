21f8c5e55b298d59f75267fcfa49ed797bbb4e6d
Ron Barrantes <ronbarrantes@gmail.com>
2017-07-27 14:58:24 -0700

    customer object fleshed out-ish

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 'use strict';
 
-function customer(firstname, lastname) {
+function Customer(product, quantity, customer, street, city,
+  state, zip, phonenumber, creditcard) {
+  this.product = product;
+  this.quantity = quantity;
+  this.customer = customer;
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.phonenumber = phonenumber;
+  this.creditcard = creditcard;
 
 }
+
+
+new Customer('ball', '3', 'Bob Robertsonburgton',
+'123 Paper St.', 'Seattle','WA','98122', '123-456-7890', '4400 1234 5678 9123');
